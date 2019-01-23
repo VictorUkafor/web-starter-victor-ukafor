8e858a522e559bc7d8f3b02a1487e87b9a7372a6
Victor Ukafor <victorukafor@gmail.com>
2019-01-23 13:37:54 +0100

    feat(restuarant-card): total redesign of cards
    - restyling restaurant for good UX/UI
    
    [Delivers #163346241]

--- src/components/Layouts/RestaurantCard.js
+++ src/components/Layouts/RestaurantCard.js
@@ -12,6 +12,11 @@ export const RestaurantCard = (props) => {
 
   const miles = Math.round(distance * 10) / 10;
 
+  let image = noImage;
+  if(images) {
+    image = images[0];
+  }
+
   let walkingTime = distance * 15;
   walkingTime = Math.round(walkingTime * 10) / 10;
 
@@ -25,31 +30,31 @@ export const RestaurantCard = (props) => {
   return (
     <div className="cards">
       <div className="card">
-        <div>
+        <div className="main-card">
           <div className="card-info">
             <i className="locator"><Place /></i>
-            <p className="first-p"> {title || 'Title of restaurant'}<br />
-              <span className="second-p">{cuisine || 'Cuisine of restaurant' }</span>
+            <p className="card-title"> {title || 'Title of restaurant'}<br />
+              <span className="rest-cuisine">{cuisine || 'Cuisine of restaurant' }</span>
             </p>
-            <div className="review-div">
+            <div className="reviews">
               <i className="star"><Star /></i>
               <p>{review} ... <span>{reviewCount}</span></p>
             </div>
           </div>
-          <div className="card-img">
-            <img src={images[0] || noImage} alt="" />
-          </div>
+          <div className="card-image" style={{backgroundImage: `url(${image})`}}> </div>
         </div>
         <div className="after-card">
-          <div className="first-div">{openClosed || 'status unknown'} <i className="dot"><FiberManualRecord /></i>{miles || 'unknown'} miles away</div>
-          <div className="second-div">
+          <div className="close-mile">{openClosed || 'status unknown'} 
+          <i className="separating-dot"><FiberManualRecord /></i>
+          {miles || 'unknown'} miles away</div>
+          <div className="walking-star">
             <div className="walking">
               <i className="walk"><DirectionsWalk /></i>
               <p className="after-card-icon-title">{walkingTime || 'unknown'}</p>
             </div>
             <div className="rating">
               <i className="rate rate-icon"><Star /></i>
-              <p className="after-card-icon-title star-rate"> {rating || 'unknown'}/<span className="rate-total">5</span></p>
+              <p className="after-card-icon-title"> {rating || 'unknown'}/<span className="rate-total">5</span></p>
             </div>
           </div>
         </div>
