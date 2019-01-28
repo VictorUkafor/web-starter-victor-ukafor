694d15988b1a2c9cedddb3b77be6215f02bd181e
Victor Ukafor <victorukafor@gmail.com>
2019-01-28 03:05:16 +0100

    feat(location-field): design the UI for the location button
    - add markup for the 'use my location' button
    - add styling to the button
    
    [Delivers #163346218]

--- src/components/Layouts/Buttons.js
+++ src/components/Layouts/Buttons.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Place from '@material-ui/icons/Place';
 import SearchField from '../../containers/SearchField';
 
 const Buttons = () => (
   <div className="buttons">
+    <div className="use-my-location">
+      <button
+        type="button"
+      >Use my location
+      </button>
+      <i className="my-location-icon"><Place /></i>
+    </div>
     <SearchField />
   </div>
 );
